420ad288b08a75d1316126798967cae9e54d7467
artemnikutov <a.nikutov@gmail.com>
2024-04-29 14:11:14 +0300

    task2

--- task02/jsrc/main/java/com/task02/HelloUrlWorld.java
+++ task02/jsrc/main/java/com/task02/HelloUrlWorld.java
@@ -2,21 +2,24 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(lambdaName = "Hello_URL_World",
+@LambdaHandler(lambdaName = "hello_world",
 	roleName = "Hello_URL_World-role",
 	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+
 )
+@LambdaUrlConfig
 public class HelloUrlWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
+
 		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
 		resultMap.put("statusCode", 200);
